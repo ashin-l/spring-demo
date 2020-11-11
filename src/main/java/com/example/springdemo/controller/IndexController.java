f47d7cbcc8c7106d03a1361292f1107b3cfaf9aa
ashin-l <lqc.ashin@gmail.com>
2020-11-11 11:54:26 +0800

    finish security test

--- src/main/java/com/example/springdemo/controller/IndexController.java
+++ src/main/java/com/example/springdemo/controller/IndexController.java
@@ -9,10 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
-@Slf4j
 public class IndexController {
 
   @Autowired
@@ -37,9 +34,25 @@ public class IndexController {
         System.out.println(user);
         return "hi man " + username;
       }
-    } catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
     return "hi man *";
   }
+
+  @GetMapping("/p1")
+  public String p1() {
+    return "in p1";
+  }
+
+  @GetMapping("/p1/hi")
+  public String hip1() {
+    return "hi in p1";
+  }
+
+  @GetMapping("/p2")
+  public String p2() {
+    return "in p2";
+  }
+
 }
--- src/main/java/com/example/springdemo/security/config/SecurityConfig.java
+++ src/main/java/com/example/springdemo/security/config/SecurityConfig.java
@@ -29,33 +29,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 // @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // @Autowired
-    // UserDetailsService userDetailsService;
-    @Autowired
-    private MyAuthenticationSuccessHandler successHandler;
-
-    // 定义用户信息服务（查询用户信息）
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("aaa").password("111").authorities("p1").build());
-        return manager;
-    }
-    // @Autowired
-    // private UserDetailsService userDetailsService;
-
-    // 密码编码器
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    // 密码编码器
-    // @Bean
-    // public PasswordEncoder passwordEncoder() {
-    // return new BCryptPasswordEncoder();
-    // }
-
     // @Override
     // protected void configure(AuthenticationManagerBuilder auth) throws Exception
     // {
@@ -63,8 +36,51 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     // }
 
-    // 配置安全拦截机制
-    // 自定义登陆
+    // ------------ 测试代码 ----------------
+    @Autowired
+    private MyAuthenticationSuccessHandler successHandler;
+
+    // 1、定义用户信息服务（查询用户信息）
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        // 角色和权限同事存在会冲突，只会保留最后调用的
+        manager.createUser(User.withUsername("aaa").password("111").roles("r1").authorities("p1").build());
+        manager.createUser(User.withUsername("bbb").password("111").authorities("p2").roles("r2").build());
+        return manager;
+    }
+
+    // 2、密码编码器
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    // 3、配置安全拦截机制
+    // 使用 spring-security 自带表单登陆
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/home", "/auth/login").permitAll().antMatchers("/p1/**")
+                .hasAuthority("p1").antMatchers("/p2/**").hasRole("r2").anyRequest().authenticated();
+        // http.httpBasic();
+        http.formLogin().loginProcessingUrl("/auth/login").successHandler(successHandler);
+
+    }
+
+    // ------------ 生产代码使用jwt ----------------
+    // // 定义用户信息服务（查询用户信息）
+    // @Autowired
+    // UserDetailsService userDetailsService;
+
+    // // 密码编码器
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    // return new BCryptPasswordEncoder();
+    // }
+
+    // // 配置安全拦截机制
+    // // 自定义登陆
     // @Override
     // protected void configure(HttpSecurity http) throws Exception {
     // http.cors().and()
@@ -88,17 +104,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // // 防止H2 web 页面的Frame 被拦截
     // http.headers().frameOptions().disable();
     // }
-
-    // 配置安全拦截机制
-    // 使用 spring-security 自带表单登陆
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("/home").permitAll().anyRequest().authenticated();
-
-        // http.httpBasic();
-        http.formLogin().loginProcessingUrl("/login").successHandler(successHandler);
-
-    }
-
 }
